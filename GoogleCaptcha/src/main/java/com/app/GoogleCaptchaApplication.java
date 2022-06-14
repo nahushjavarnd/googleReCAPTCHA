0201cffb745eae207f150aa62201d9e1c9e58ebb
nahushjavarnd <nahush.javarnd@gmial.com>
2022-06-14 03:21:34 -0400

    First Commite

---
+++ GoogleCaptcha/src/main/java/com/app/GoogleCaptchaApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleCaptchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleCaptchaApplication.class, args);
+	}
+
+}
