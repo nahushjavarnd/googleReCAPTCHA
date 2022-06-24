950e119c3905815cca7e39293017f552930e3940
nahushjavarnd <nahush.javarnd@gmial.com>
2022-06-24 16:16:37 -0400

    theory update

--- GoogleCaptcha/src/main/java/com/app/GoogleCaptchaApplication.java
+++ GoogleCaptcha/src/main/java/com/app/GoogleCaptchaApplication.java
@@ -3,7 +3,19 @@ package com.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+// @SpringBootApplication annotation and invoke SpringApplication.run() method.
+
+
+// Spring Boot @SpringBootApplication annotation is used to mark a configuration class that declares one or 
+//more @Bean methods and also triggers auto-configuration and component scanning. 
+//It’s same as declaring a class with @Configuration, @EnableAutoConfiguration and @ComponentScan annotations.
+
+
+@SpringBootApplication // (@configurtion + @EnableAutoConfigurtion + @Componentscan)
+// 1) It will behave act as bean.
+// 2) example: Spring Boot auto-configuration can automatically configure the Thymeleaf template resolver
+// 3) It is mainly used to scan the classes and packages to create the bean.
 public class GoogleCaptchaApplication {
 
 	public static void main(String[] args) {
--- GoogleCaptcha/src/main/java/com/app/controller/UserController.java
+++ GoogleCaptcha/src/main/java/com/app/controller/UserController.java
@@ -16,20 +16,39 @@ import com.app.service.UserService;
 import com.app.validator.CaptchaValidator;
 
 @Controller
+//The @Controller annotation indicates that a particular class serves the role of a controller.
+// There is no need to extend any controller base class or reference the Servlet API.
+
+
+// @RequestMapping — the HTTP Method
+// The HTTP method parameter has no default. So, if we don't specify a value, it's going to map to any HTTP request.
+
+
+
+
+// mapping an HTTP request to a method using some basic criteria.
 @RequestMapping("/user")
 public class UserController {
 	
+	
+	// The @Autowired annotation marks a Constructor, Setter method, Properties and Config() method as to be autowired
+	//that is ‘injecting beans'(Objects) at runtime by Spring Dependency Injection mechanism which is clearly depicted from the image below as shown:
 	@Autowired
 	private UserService service;
 	@Autowired
 	private CaptchaValidator validator;
 	
-	@GetMapping("/register")
+	
+	
+	@GetMapping("/register") // Getmapping is a Spring notation and is widely used in mapping HTTP GET requests
 	public String registerUser(Model model) {
 		model.addAttribute("user", new User());
 		return "registerUser";
 	}
 	
+	
+	//The @PostMapping is specialized version of @RequestMapping annotation that acts as a shortcut for @RequestMapping(method = RequestMethod. POST) . 
+	// The @PostMapping annotated methods in the @Controller annotated classes handle the HTTP POST requests matched with given URI expression.
 	
 	@PostMapping("/save")
 	public String saveUser(@ModelAttribute User user, Model model, @RequestParam("g-recaptcha-response") String captcha) {
@@ -45,6 +64,10 @@ public class UserController {
 		}
 		return "registerUser";
 	}
+	
+	
+	// Annotation Type GetMapping. Annotation for mapping HTTP GET requests onto specific handler methods.
+	// Specifically, @GetMapping is a composed annotation that acts as a shortcut for @RequestMapping(method = RequestMethod. GET) 
 	
 	@GetMapping("/all")
 	public String getAllUsers(Model model) {
